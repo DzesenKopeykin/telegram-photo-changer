621dbe9f758dfc80c7bb9cee395ea068305e50ec
DzesenKopeykin <kopeykindzzesen@gmail.com>
2022-01-05 18:50:31 +0300

    install pyrogram

--- run.py
+++ run.py
@@ -1,10 +1,15 @@
 import os
 
 from dotenv import load_dotenv
+from pyrogram import Client
 
 
 if __name__ == "__main__":
     print("Running...")
 
     load_dotenv(".env")
-    print(os.environ["TELEGRAM_API_ID"])
+    api_id = os.environ["TELEGRAM_API_ID"]
+    api_hash = os.environ["TELEGRAM_API_HASH"]
+
+    with Client("telegram_client", api_id, api_hash) as client:
+        client.send_message("me", "Hi!!!")
