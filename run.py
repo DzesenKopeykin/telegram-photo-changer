8ba6959e6da5b5b771f26087dab4a9893b26fd6e
DzesenKopeykin <kopeykindzzesen@gmail.com>
2022-01-06 14:42:00 +0300

    set new profile photo via pillow

--- run.py
+++ run.py
@@ -1,7 +1,25 @@
+from io import BytesIO
+
+from PIL import Image, ImageDraw, ImageFont
+from pyrogram import Client
+
 from client import client
+
+
+def change_photo(client: Client) -> None:
+    image = Image.new("RGB", (1024, 1024), (0, 0, 0))
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype("Ubuntu-Regular.ttf", 20)
+    draw.text((10, 10), "Hello World", font=font, fill=(255, 255, 255))
+    
+    fp = BytesIO()
+    image.save(fp, "png")
+    fp.name = "1.png"
+    client.set_profile_photo(photo=fp)
 
 
 if __name__ == "__main__":
     print("Running...")
 
-    client.run()
+    with client:
+        change_photo(client)
